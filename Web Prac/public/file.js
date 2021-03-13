a255c5ca914360dd74dc74dd49b3945c82b694eb
Shaurya Aher <72655598+shauryaaher@users.noreply.github.com>
2021-03-13 14:22:12 +0530

    Update file.js

--- Web Prac/public/file.js
+++ Web Prac/public/file.js
@@ -1,1 +1,38 @@
-//Ting tong
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCtvhW0qWpIJn41bx54xTxgxVEmRFwAW-s",
+  authDomain: "web-prac-12a96.firebaseapp.com",
+  projectId: "web-prac-12a96",
+  storageBucket: "web-prac-12a96.appspot.com",
+  messagingSenderId: "947491979867",
+  appId: "1:947491979867:web:56633f2cc68459f30bb57e",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const authProvider = new firebase.auth.GoogleAuthProvider();
+
+googleSignIn.addEventListener("click", () => {
+  auth
+    .signInWithPopup(authProvider)
+    .then((user) => {
+      console.log("Successfully signed in");
+      console.log(user);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+signOut.addEventListener("click", () => {
+  auth
+    .signOut()
+    .then(() => {
+      console.log("Successfully signed out");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
