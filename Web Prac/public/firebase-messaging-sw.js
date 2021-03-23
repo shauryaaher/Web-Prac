417762e0801c00668b822b87cee235d670d70fa6
Shaurya Aher <72655598+shauryaaher@users.noreply.github.com>
2021-03-23 08:59:55 +0530

    Update firebase-messaging-sw.js

--- Web Prac/public/firebase-messaging-sw.js
+++ Web Prac/public/firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js");
-
+//Please put your own Firebase config.
 var firebaseConfig = {
     apiKey: "AIzaSyCtvhW0qWpIJn41bx54xTxgxVEmRFwAW-s",
     authDomain: "web-prac-12a96.firebaseapp.com",
